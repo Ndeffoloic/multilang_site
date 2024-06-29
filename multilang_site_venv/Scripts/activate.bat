fde4f4b8e572e2cc7879ce41ee893fb7cad09444
nembotnd1u <nembotnd1u@etu.univ-lorraine.fr>
2024-06-29 18:33:54 +0200

    renommage de l'env virtuel

--- multilang_site_venv/Scripts/activate.bat
+++ multilang_site_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\loicn\Documents\BOURSE\Alternance\Entreprises\dioci\multilang_site\multilang_site
+set VIRTUAL_ENV=C:\Users\loicn\Documents\BOURSE\Alternance\Entreprises\dioci\multilang_site\multilang_site_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(multilang_site) %PROMPT%
+set PROMPT=(multilang_site_venv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(multilang_site) 
+set VIRTUAL_ENV_PROMPT=(multilang_site_venv) 
 
 :END
 if defined _OLD_CODEPAGE (
